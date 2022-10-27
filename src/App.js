35cb79d6b61e0858f4da3861d330e57e35096e11
Sango <60422984+MatyilaSango@users.noreply.github.com>
2022-10-27 20:00:53 +0200

    added nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import gitPic from './icons/GitHub-Mark-Light-32px.png';
+import linkedinPic from './icons/linkedin.png'
 
 function App() {
   return (
     <div className="container">
-      <nav>
-        
-      </nav>
+      <div className="nav">
+        <span className="left-span">SM</span>
+        <span className="mid-span">SANGO MATYILA MINI PORTFOLIO</span>
+        <span className="rigth-span">
+          <a href="https://github.com/MatyilaSango"><img src={gitPic} alt="pic" /></a>
+          <a href="https://www.linkedin.com/in/sango-matyila-b1584a237/"><img src={linkedinPic} alt="pic" /></a>
+        </span>
+      </div>
+
+
     </div>
   );
 }
